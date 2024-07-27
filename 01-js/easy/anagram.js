88a0e743ecec5eb74bbc1b0f53285cd1d0202bbd
Satnammeena2004 <satnam.meena2004@gmail.com>
2024-07-28 00:21:45 +0530

    anagram problem bug fi*

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,11 +5,7 @@
 */
 
 function isAnagram(str1, str2) {
-<<<<<<< HEAD
-console.log("string")
-=======
-console.log("practice pull request");
->>>>>>> 8e020727aecc4c85013561e7b49600ad1a43fc24
+   return str1.split("").sort().join("")===str2.split("").sort().join("")
 }
 
 module.exports = isAnagram;
